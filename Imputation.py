1d272898bb81f77e80b085ca2750bbbac500f9c8
onurserce <onurserce@gmail.com>
2022-07-04 16:10:56 +0200

    Implemented tracking of Imputed vs NonImputed protein groups.

--- Imputation.py
+++ Imputation.py
@@ -13,26 +13,38 @@ from tqdm import tqdm
 PgMatrix = ReadPgMatrix("/Users/onurserce/Desktop/THP1 proteome/TidyPgMatrix.hdf")
 
 
-def ImputeFromReplicates(PgMatrix):
+def ImputeFromReplicates(PgMatrix, ThresholdRatio=0.65):
     """Imputation from replicates."""
-    df = PgMatrix.sort_index(axis=1)
+    PgMatrix = PgMatrix.sort_index(axis=1)
+    NaBeforeImputation = PgMatrix.isna()
     # Raise exception if the DataFrame doesn't have 'Replicates' column.
-    if 'Replicate' not in df.columns.names:
+    if 'Replicate' not in PgMatrix.columns.names:
         raise Exception("'Replicate' column not found, aborting imputation!!!")
 
-    ReplicateLevel = df.columns.names.index('Replicate')
-    Unique = df.columns.droplevel(ReplicateLevel).unique()
+    ReplicateLevel = PgMatrix.columns.names.index('Replicate')
+    Unique = PgMatrix.columns.droplevel(ReplicateLevel).unique()
 
     for U in tqdm(Unique,
                   desc="Imputing missing data using 'Replicate' averages"):
-        ImputedSlice = df.loc[:, U].where(
-            df.loc[:, U].notna(),
-            df.loc[:, U].mean(axis=1),
-            axis=0)
+        Slice = PgMatrix.loc[:, U]  # Get the current group of replicates
+        NReplicates = len(PgMatrix.loc[:, U].columns)  # Count of replicates
+        OverThreshold = (
+            Slice.notna().sum(axis=1) / NReplicates
+            ) >= ThresholdRatio  # Check if the row is above the threshold
+        ToBeImputedMask = Slice.notna()[OverThreshold]  # Create a mask
+
+        ImputedSlice = Slice.where(
+            ToBeImputedMask,
+            Slice.mean(axis=1),
+            axis=0).to_numpy()  # Imputation with the mean
+        PgMatrix.loc[:, U] = ImputedSlice  # Back into the DataFrame
         # Conversion to NDArray is necessary!
-        df.loc[:, U] = ImputedSlice.to_numpy()
-# TODO: Keep track of the imputed values!
-    return df
+
+        NaAfterImputation = PgMatrix.isna()
+        Imputed = NaAfterImputation != NaBeforeImputation
+        NotImputed = NaAfterImputation & NaBeforeImputation
+
+    return PgMatrix, Imputed, NotImputed
 
 
 def ImputeWithDownshift():
